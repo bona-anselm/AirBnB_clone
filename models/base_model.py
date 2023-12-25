ac46976e75e98e64698ddd6251cce9aaa331fbc8
Bonaventure Anselm <bona.anselm@gmail.com>
2023-12-25 10:58:31 +0100

    Task 3

---
+++ models/base_model.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" Defines the class BaseModel which all other model will inherit """
+
+import uuid
+from datetime import datetime
+
+class BaseModel():
+    """ Defines all common attributes/methods for other classes """
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        return f'[{self.__class__.__name__}] ({self.id}) {self.__dict__}'
+
+    def save(self):
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        new_dict = self.__dict__.copy()
+        new_dict['__class__'] = self.__class__.__name__
+        new_dict['created_at'] = self.created_at.isoformat()
+        new_dict['updated_at'] = self.updated_at.isoformat()
+        return new_dict
+    #print(new_dict)
+
+
